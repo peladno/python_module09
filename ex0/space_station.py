445104baa7faf4dab3994ac74b77e7fbea138fa7
peladno <j.perezurrutia@gmail.com>
2026-07-23 18:39:18 +0900

    ellipsis added

--- ex0/space_station.py
+++ ex0/space_station.py
@@ -4,11 +4,11 @@ from datetime import datetime
 
 
 class SpaceStation(BaseModel):
-    station_id: str
-    name: str = Field(min_length=1, max_length=30)
-    crew_size: int = Field(ge=1, le=20)
-    power_level: float = Field(ge=0.0, le=100.0)
-    oxygen_level: float = Field(ge=0.0, le=100.0)
+    station_id: str = Field(..., min_length=1, max_length=30)
+    name: str = Field(..., min_length=1, max_length=50)
+    crew_size: int = Field(..., ge=1, le=20)
+    power_level: float = Field(..., ge=0.0, le=100.0)
+    oxygen_level: float = Field(..., ge=0.0, le=100.0)
     last_maintenance: datetime
     is_operational: bool = Field(default=True)
     notes: str | None = None
